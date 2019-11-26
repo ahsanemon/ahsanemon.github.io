be388d724d5da74511be7a73c1d35abf4d942e7f
Ahsan Ullah Ahmad <ahsanullah.ahmad@realdigital.de>
2019-11-26 19:06:32 +0100

    send and receive message

--- chat/chat.js
+++ chat/chat.js
@@ -28,26 +28,106 @@ var Chat = {
 			Chat.sendMessage(data);
 			// console.log('My form was sent');
 		});
+
+		document.querySelectorAll('.actions-bar button').forEach(function(btn){
+			btn.addEventListener('click', function(event){
+				Chat.performAction(event.target.dataset.action);
+			});
+		});
+
+		Chat.pollMessages(); // as first attempt will be after the interval sec so we manually started at the begining.
+		
+		Chat._pollInterval = setInterval(Chat.pollMessages, 3000);
+		
+		// let interval = setInterval(Chat.pollMessages, 10000);
+		// clearInterval(interval);
 	},
 
-	sendMessage: function(messageData){
-		console.log('Sending message', messageData);
-		// messageData.author_color = messageData.author_color.replace('#','');
-		console.log('Sending message', messageData);
+	_actionHandler: {
+		clearLog: function(){
+			document.querySelector('.chat-log').innerHTML = '';
+		},
+		toggleTimestamps: function(){
+			document.querySelector('.chat-log').classList.toggle('no-timestamps');
+		},
 	},
+
+	performAction: function(actionName){
+		console.log('action performed:', actionName);
+
+		if (typeof Chat._actionHandler[actionName] === "function"){
+			Chat._actionHandler[actionName]();
+		} else {
+			console.error('Unknown action', actionName);
+		}
+
+	},
+
+	_API_ROOT: 'https://real-webclass.gmetz.fr/chat/',
+
+	sendMessage: async function(messageData){
+
+		// let body = new URLSearchParams(messageData).toString();
+
+		console.log('Sending message', messageData);
+
+		Chat._API_ROOT
+		let response = await fetch(Chat._API_ROOT + 'send.php', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: new URLSearchParams(messageData).toString()
+		});
+
+		Chat.pollMessages();
+
+
+
+		// messageData.author_color = messageData.author_color.replace('#','');
+		// console.log('Sending message', messageData);
+	},
+
+	_latestId:0,
+
+	pollMessages: async function(){
+		console.log('Polling message');
+
+
+		let response = await fetch(Chat._API_ROOT + 'get_messages.php?after_id='+Chat._latestId);
+		let data = await response.json();
+
+		if (!data.messages || data.messages.length == 0)
+			return;
+
+		const chatLog = document.querySelector('.chat-log');
+
+		data.messages.forEach(function(message){
+			if (message.id <= Chat._latestId)
+				return;
+
+			chatLog.insertAdjacentHTML('beforeend', `
+				<p class="message">
+					<span class="timestamp">${message.timestamp}</span>
+					<span class="author-name">${message.author_name}</span> <span class="verb">:</span>
+					<span class="content">${message.message}</span>
+				</p>
+
+				`);
+		});
+
+		Chat._latestId = data.messages[data.messages.length - 1].id
+
+		chatLog.scrollTop = chatLog.scrollHeight
+		// console.log('got response', response);
+	}
+
 };
 
-document.addEventListener('DOMContentLoaded', Chat.init);
+document.addEventListener('DOMContentLoaded', function(event){
+	Chat.init();
 
-function toggleTimestamp() {
-	var x = document.getElementsByClassName('timestamp');
-	var element;
-	for (element = 0; element < x.length; element++) {
+	// document.querySelector('#toggleButton').addEventListener
+});
 
-		if (x[element].style.display === "none") {
-			x[element].style.display = "block";
-		} else {
-			x[element].style.display = "none";
-		}
-	}
-}
+
--- home work.md
+++ home work.md
@@ -3,3 +3,13 @@ make the input bar look nicer
 	- add margin between elements
 	- when page loads username inputs must be focused (use javascript)
 	- make the color button nicer (circular may be)
+
+
+	- clear the message field when the messaeg field is sent
+	- use the author color with the text
+	- replace string concats with template literals
+	- click on username to prefill message
+	- highlight message on @username mention indexof() or regex
+	- clickable link, clickable @usernames
+	- button to toggle dark mode
+	- escape the html 
